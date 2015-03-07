513741bc822623db59170ea56ee97ec1b2da5de5
Tom Clark <tom.clark@op.ac.nz>
2015-03-07 15:51:28 +1300

    person passign test_increment_age

--- oosd/week03/unit_testing_solutions/person.py
+++ oosd/week03/unit_testing_solutions/person.py
@@ -7,3 +7,10 @@ class Person:
     def change_name(self, first, last):
         self.first_name = first
         self.last_name= last
+
+    def increment_age(self):
+        try:
+            self.age += 1
+        except AttributeError:
+            self.age = 1
+        return self.age
