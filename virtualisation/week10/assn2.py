e38638cc007d43ef8813ce822c2fa4b87471fefb
Tom Clark <tom@runawayplay.com>
2018-09-25 07:40:19 +1200

    added assignment 2 stuff

--- virtualisation/week10/assn2.py
+++ virtualisation/week10/assn2.py
@@ -1,15 +1,18 @@
 import argparse
-import openstack
+#import openstack
 
 # create openstack connection using credentials from clouds.yml
 #conn = 
 
 # get command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('operation', help='Either "up" or "down"')
+parser.add_argument('operation', help='One of "report",  "up" or "down"')
 args = parser.parse_args()
 
-if args.operation == "up":
+if args.operation == 'report':
+    # print a report on instances
+    pass
+elif args.operation == 'up':
     # bring up various openstack resources
     pass 
 elif args.operation == 'down':
